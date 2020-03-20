454539cb733eeb94e5457e65da4c8c328348d258
Benny Gächter <benny.gaechter@gmail.com>
2020-03-20 11:48:05 +0100

    changed response type to json

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 )
 
 
+type ResponseObject struct {
+	Message string
+}
+
 func main() {
 
 	responseHandler := func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintf(w, "%s\n", os.Getenv("MESSAGE"))
+		res, err := json.Marshal(ResponseObject{Message: os.Getenv("MESSAGE")})
+		if err != nil {
+			log.Fatal(err)
+		}
+		w.Write(res)
 	}
 
 	http.HandleFunc("/", responseHandler)
